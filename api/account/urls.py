93be5831cbdcb19ed1f96232e1fcfc5294dc1f2f
Nimrod León <nleonc14@gmail.com>
2024-01-15 12:00:21 -0500

    configuración básica de usuarios

---
+++ api/account/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
