6e986b9e164a15fcd4ad0c2244b17e889fb7d1c2
Nimrod León <nleonc14@gmail.com>
2024-01-19 16:11:58 -0500

    modelo empresa

--- api/account/models.py
+++ api/account/models.py
@@ -1,3 +1,11 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Empresa(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    ruc = models.CharField(max_length=15, null=True)
+    rzn_social = models.CharField(max_length=256, null=True)
+    address = models.CharField(max_length=256, null=True)
+    celular = models.CharField(max_length=15, null=True)
