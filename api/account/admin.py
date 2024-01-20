a92b06ba8e634a36bbf06eafa23511dcb4184705
Nimrod León <nleonc14@gmail.com>
2024-01-20 10:36:57 -0500

    sucursales

--- api/account/admin.py
+++ api/account/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Empresa
+from .models import Empresa, Sucursal
 
 
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
-    list_display = ['id', 'ruc', 'rzn_social', 'address', 'celular']
+    list_display = ['id', 'ruc', 'rzn_social', 'direccion', 'celular']
     search_fields = ['ruc', 'rzn_social', 'address', 'celular']
+
+
+@admin.register(Sucursal)
+class SucursalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cod_sunat', 'nombre', 'direccion', 'descripcion')
+    search_fields = ('nombre', 'cod_sunat',)
+    list_filter = ('empresa',)
--- api/account/models.py
+++ api/account/models.py
@@ -7,5 +7,19 @@ class Empresa(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     ruc = models.CharField(max_length=15, null=True)
     rzn_social = models.CharField(max_length=256, null=True)
-    address = models.CharField(max_length=256, null=True)
+    direccion = models.CharField(max_length=256, null=True)
     celular = models.CharField(max_length=15, null=True)
+    sucursal_por_defecto = models.UUIDField(null=True)
+
+
+class Sucursal(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    cod_sunat = models.CharField(max_length=64, null=True)
+    nombre = models.CharField(max_length=256, null=True)
+    direccion = models.CharField(max_length=256, null=True)
+    descripcion = models.CharField(max_length=256, null=True)
+    nombre_contacto = models.CharField(max_length=256, null=True)
+    telefono_contacto = models.CharField(max_length=256, null=True)
+    correo_contacto = models.CharField(max_length=256, null=True)
+    almacen = models.UUIDField(null=True)
