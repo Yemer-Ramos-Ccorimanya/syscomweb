932a24b73cac364488a3cef18acd9a656e763b72
Nimrod León <nleonc14@gmail.com>
2024-01-19 22:37:09 -0500

    tabla alquileres

--- api/alquileres/admin.py
+++ api/alquileres/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Alquiler
 
-# Register your models here.
+
+@admin.register(Alquiler)
+class AlquilerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cliente_id', 'monto_total', 'fecha_alquiler', 'fecha_devolucion')
+    search_fields = ('id', 'cliente_id__nombre_completo', 'direccion_evento')
+    list_filter = ('empresa_id', 'fecha_alquiler', 'es_una_reservacion')
