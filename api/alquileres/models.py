34660545ab723999e12a0a8f7d63fdd0e5820863
Nimrod León <nleonc14@gmail.com>
2024-01-19 22:06:49 -0500

    generar módulo de clientes

--- api/alquileres/models.py
+++ api/alquileres/models.py
@@ -1,3 +1,8 @@
+import uuid
 from django.db import models
+from account.models import Empresa
 
-# Create your models here.
+
+class Alquiler(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    empresa_id = models.ForeignKey(Empresa, on_delete=models.CASCADE)
