572aecf7d50148fe2d087f044454678d66f789ec
Yemer-Ramos-Ccorimanya <1005920191@unajma.edu.pe>
2024-01-21 14:00:37 -0500

    refacturizacion tabla producto

--- api/inventarios/models.py
+++ api/inventarios/models.py
@@ -8,35 +8,45 @@ class Categoria(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=256, null=True)
 
+class SubCategoria(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    categoria= models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=256, null=True)
 
 class Atributo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=256, null=True)
 
-
-ESTADO_CHOICES_PRODUCTO = [
+"""
+GESTION_INVENTARIO_PRODUCTO = [
     ('DISPONIBLE', 'Disponible'),
     ('ALQUILADO', 'Alquilado'),
     ('EN_MANTENIMIENTO', 'En Mantenimiento'),
 ]
-
+"""
 
 class Producto(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=256, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
+    Sub_categoria=models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
     description = models.CharField(max_length=256, null=True)
     precio_unitario = models.DecimalField(max_digits=15, decimal_places=4, null=True)
-    precio_por_volumen = models.DecimalField(max_digits=15, decimal_places=4, null=True)
-    cantidad_min_volumen = models.IntegerField(null=True)
-    estado = models.CharField(max_length=256, choices=ESTADO_CHOICES_PRODUCTO)
-    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
-
+    unidad_medida =models.CharField(max_length=256, null=True)
+    impuesto = models.CharField(max_length=256, null=True)
+    codigo_barra= models.CharField(max_length=64, null=True)
+    tipo_producto =  models.CharField(max_length=256, null=True)
+    gestion_inventario= models.CharField(max_length=256, null=True)
+    
 
 class AtributoProducto(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
-    atributo = models.ForeignKey(Atributo, on_delete=models.SET_NULL, null=True, blank=True)
     producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True, blank=True)
-    valor_atributo = models.CharField(max_length=256, null=True)
+    nombre = models.CharField(max_length=256, null=True)
+
+class ProductoTerminal(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True, blank=True)
+    nombre = models.CharField(max_length=256, null=True)
