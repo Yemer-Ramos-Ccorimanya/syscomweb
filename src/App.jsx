22620817da8f6810e84c357d05978e18dfc356d5
josedantearroyo <malekframi@gmail.com>
2024-01-20 21:51:17 -0500

    header-sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './components/auth/login.component'
 import SignUp from './components/auth/signup.component'
+import Header from './components/header.component'
+import Sidebar from './components/sidebar.component'
 function App() {
   return (
     <Router>
+      <Header />
+      <Sidebar />
       <div className="App">
         <div className="auth-wrapper">
           <div className="auth-inner">
