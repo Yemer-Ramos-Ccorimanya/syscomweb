b7a617296bf4fcca014e265939ee11b0e7ac517d
Nimrod León <nleonc14@gmail.com>
2024-01-21 22:19:50 -0500

    refactoring modelos inventario

--- api/inventarios/models.py
+++ api/inventarios/models.py
@@ -16,7 +16,7 @@ class CatalogoSku(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=256, null=True)
-    codigo_sku = models.CharField(max_length=256, null=True)
+    codigo_sku = models.CharField(max_length=256, null=True, unique=True)
     unidad_medida = models.CharField(max_length=256, null=True)
     descripcion = models.CharField(max_length=256, null=True)
     estado = models.BooleanField(default=True)
@@ -51,13 +51,7 @@ class Categoria(models.Model):
 
 class SubCategoria(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    categoria= models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=256, null=True)
-
-
-class Atributo(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
     nombre = models.CharField(max_length=256, null=True)
 
 
@@ -66,10 +60,10 @@ class Producto(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=256, null=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
-    Sub_categoria=models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
-    description = models.CharField(max_length=256, null=True)
+    sub_categoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=256, null=True)
     precio_unitario = models.DecimalField(max_digits=15, decimal_places=4, null=True)
-    unidad_medida =models.CharField(max_length=256, null=True)
+    unidad_medida = models.CharField(max_length=256, null=True)
     impuesto = models.CharField(max_length=256, null=True)
     codigo_barra= models.CharField(max_length=64, null=True)
     tipo_producto =  models.CharField(max_length=256, null=True)
@@ -84,30 +78,31 @@ class AtributoProducto(models.Model):
 
 class ValorAtributoProducto(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    Atributo_producto = models.ForeignKey(AtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
+    atributo_producto = models.ForeignKey(AtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
     valor = models.CharField(max_length=256, null=True)
 
 
 class VariacionAtributoProducto(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    Atributo_producto = models.ForeignKey(AtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
-    Valor_atributo_producto = models.ForeignKey(ValorAtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
-    precio_unitario= models.CharField(max_length=256, null=True)
+    producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True, blank=True)
+    atributo_producto = models.ForeignKey(AtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
+    valor_atributo_producto = models.ForeignKey(ValorAtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
+    precio_unitario = models.DecimalField(max_digits=15, decimal_places=4, null=True)
     codigo_barra = models.CharField(max_length=64, null=True)
 
 
 class VariacionAtributoProductoSku(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    variacion_atributo_producto = models.ForeignKey(ValorAtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
-    catalogo_sku=  models.ForeignKey(CatalogoSku, on_delete=models.SET_NULL, null=True, blank=True)
+    variacion_atributo_producto = models.ForeignKey(VariacionAtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
+    catalogo_sku = models.ForeignKey(CatalogoSku, on_delete=models.SET_NULL, null=True, blank=True)
     cantidad_descontada = models.CharField(max_length=256, null=True)
 
 
 class VariacionAtributoProductoTerminal(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    variacion_atributo_producto = models.ForeignKey(ValorAtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
-    catalogo_sku = models.ForeignKey(CatalogoSku, on_delete=models.SET_NULL, null=True, blank=True)
-    cantidad_descontada = models.CharField(max_length=256, null=True)
+    variacion_atributo_producto = models.ForeignKey(VariacionAtributoProducto, on_delete=models.SET_NULL, null=True, blank=True)
+    terminal = models.ForeignKey(Terminal, on_delete=models.SET_NULL, null=True, blank=True)
+    precio_unitario = models.DecimalField(max_digits=15, decimal_places=4, null=True)
 
 
 class ProductoTerminal(models.Model):
@@ -122,5 +117,3 @@ class ProductoSku(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True, blank=True)
     catalogo_sku = models.ForeignKey(CatalogoSku, on_delete=models.SET_NULL, null=True, blank=True)
     cantidad_descontada = models.IntegerField(default=0)
-    terminal=  models.ForeignKey(Terminal, on_delete=models.SET_NULL, null=True, blank=True)
-    precio= models.CharField(max_length=256, null=True)
