c7d6fb12dcee14c2aba65c65811f4e47e86e8fa6
Nimrod León <nleonc14@gmail.com>
2024-01-24 10:54:56 -0500

    configuración axios

--- api/account/urls.py
+++ api/account/urls.py
@@ -2,9 +2,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView
 
-
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
