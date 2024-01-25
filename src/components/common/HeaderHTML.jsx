f20ba7cb082ddc38a52fef14e8e675cfcf6838af
Nimrod León <nleonc14@gmail.com>
2024-01-25 11:47:56 -0500

    user context created

--- src/components/common/HeaderHTML.jsx
+++ src/components/common/HeaderHTML.jsx
@@ -2,15 +2,18 @@ import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import logo from "../../assets/logo.png"
-import {Button} from "react-bootstrap"
-import {Link, useNavigate} from "react-router-dom"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons"
-import {faCircleUser} from "@fortawesome/free-regular-svg-icons"
+import { Button } from "react-bootstrap"
+import { Link, useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons"
+import { UserContext } from "../../context/UserProvider"
+import { useContext } from "react"
 
 export const HeaderHTML = () => {
   const navigate = useNavigate()
-  
+  const { userData } = useContext(UserContext)
+
   const handleLogout = () => {
     if (localStorage.getItem("AuthToken"))
       localStorage.removeItem("AuthToken")
@@ -23,9 +26,9 @@ export const HeaderHTML = () => {
     <Navbar expand="lg" className="bg-body-tertiary shadow-sm">
       <Container fluid={true}>
         <Link to={"/"} className="navbar-brand">
-          <img src={logo}/>
+          <img src={logo} />
         </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="d-lg-none me-auto">
             <Nav.Link href="#home" className="text-uppercase">Inicio</Nav.Link>
@@ -40,16 +43,16 @@ export const HeaderHTML = () => {
           <Nav className="ms-auto">
             <Nav.Link href="#home">
               <div className="d-flex flex-column align-items-end">
-                <span>Karina Cardenas Casas</span>
-                <span className="text-primary text-uppercase">Empresa de la tienda de ropa KYLOS'S</span>
+                <span>{userData.user?.first_name} {userData.user?.last_name}</span>
+                <span className="text-primary text-uppercase">{userData.empresa?.rzn_social}</span>
               </div>
             </Nav.Link>
             <Nav.Link className="d-none d-lg-block">
-              <FontAwesomeIcon icon={faCircleUser} style={{fontSize: "3rem"}}/>
+              <FontAwesomeIcon icon={faCircleUser} style={{ fontSize: "2.8rem" }} />
             </Nav.Link>
             <div className="d-flex align-items-center justify-content-end">
               <Button variant="danger" onClick={handleLogout} className="px-3">
-                <FontAwesomeIcon icon={faRightFromBracket} className="me-1"/>
+                <FontAwesomeIcon icon={faRightFromBracket} className="me-1" />
                 <span className="text-uppercase">Cerrar Sessión</span>
               </Button>
             </div>
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,45 +1,48 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import {Login} from "../components/auth/Login"
-import {Registro} from "../components/auth/Registro"
-import {Dashboard} from "./Dashboard"
-import {InventarioRoute} from "./InventarioRoute"
-import {ClienteRoute} from "./clienteRoute"
-import {AlquilerRoute} from "./AlquilerRoute"
-import {PuntodeVentaRoute} from "./PuntodeVentaRoute"
-import {PrivateRoute} from "./PrivateRoute"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Login } from "../components/auth/Login"
+import { Registro } from "../components/auth/Registro"
+import { Dashboard } from "./Dashboard"
+import { InventarioRoute } from "./InventarioRoute"
+import { ClienteRoute } from "./clienteRoute"
+import { AlquilerRoute } from "./AlquilerRoute"
+import { PuntodeVentaRoute } from "./PuntodeVentaRoute"
+import { PrivateRoute } from "./PrivateRoute"
+import { UserProvider } from "../context/UserProvider"
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/registro" element={<Registro/>}/>
-        <Route path="/inventarios/*" element={
-          <PrivateRoute>
-            <InventarioRoute/>
-          </PrivateRoute>
-        }/>
-        <Route path="/clientes/*" element={
-          <PrivateRoute>
-            <ClienteRoute/>
-          </PrivateRoute>
-        }/>
-        <Route path="/alquileres/*" element={
-          <PrivateRoute>
-            <AlquilerRoute/>
-          </PrivateRoute>
-        }/>
-        <Route path="/PuntodeVenta/*" element={
-          <PrivateRoute>
-            <PuntodeVentaRoute/>
-          </PrivateRoute>
-        }/>
-        <Route path="/*" element={
-          <PrivateRoute>
-            <Dashboard/>
-          </PrivateRoute>
-        }/>
-      </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/inventarios/*" element={
+            <PrivateRoute>
+              <InventarioRoute />
+            </PrivateRoute>
+          } />
+          <Route path="/clientes/*" element={
+            <PrivateRoute>
+              <ClienteRoute />
+            </PrivateRoute>
+          } />
+          <Route path="/alquileres/*" element={
+            <PrivateRoute>
+              <AlquilerRoute />
+            </PrivateRoute>
+          } />
+          <Route path="/PuntodeVenta/*" element={
+            <PrivateRoute>
+              <PuntodeVentaRoute />
+            </PrivateRoute>
+          } />
+          <Route path="/*" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   )
 }
