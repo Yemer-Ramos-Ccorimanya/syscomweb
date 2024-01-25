14cf41753c9ad307f8c4697832683a2da229d6da
Nimrod León <nleonc14@gmail.com>
2024-01-25 05:44:31 -0500

    refactoring modelo cliente

--- api/clientes/models.py
+++ api/clientes/models.py
@@ -2,11 +2,20 @@ import uuid
 from django.db import models
 from account.models import Empresa
 
+TIPO_DOCUMENTO_CHOICES = [
+    ('0', 'SIN DEFINIR'),
+    ('1', 'D.N.I'),
+    ('6', 'R.U.C'),
+    ('7', 'PASAPORTE'),
+]
+
 
 class Cliente(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    tipo_doc = models.CharField(max_length=1, choices=TIPO_DOCUMENTO_CHOICES, null=True)
     dni = models.CharField(max_length=15, null=True)
     nombre_completo = models.CharField(max_length=256, null=True)
-    address = models.CharField(max_length=256, null=True)
-    celular = models.CharField(max_length=15, null=True)
+    address = models.CharField(max_length=256, null=True, blank=True)
+    cod_ubigeo = models.CharField(max_length=256, null=True, blank=True)
+    celular = models.CharField(max_length=15, null=True, blank=True)
