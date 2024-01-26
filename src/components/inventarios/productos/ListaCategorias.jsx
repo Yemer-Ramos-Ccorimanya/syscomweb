167c75f6405304c01fe38458cf7b8e96cb8e59fb
Nimrod León <nleonc14@gmail.com>
2024-01-25 19:58:44 -0500

    lista de categorias y subcategorias

--- src/components/inventarios/productos/ListaCategorias.jsx
+++ src/components/inventarios/productos/ListaCategorias.jsx
@@ -1,40 +1,59 @@
-import { Card, Form, InputGroup, Button, Modal } from "react-bootstrap"
+import { Card, Form, InputGroup, Button } from "react-bootstrap"
 import { MainContainer } from "../../common/MainContainer"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { CategoriaModal } from "./CategoriaModal"
+import { getCategoriasHook } from "../../../hooks/categorias"
+import { useFormik } from "formik"
 
 export const ListaCategorias = () => {
-  const [showCategoryModal, setShowCategoryModal] = useState(false);
-  const handleShowCategory = () => setShowCategoryModal(true);
-  const handleCloseCategory = () => setShowCategoryModal(false);
+  const [categorias, setCategorias] = useState({})
+  const [showCategoryModal, setShowCategoryModal] = useState(false)
+  const handleShowCategory = () => setShowCategoryModal(true)
+  const handleCloseCategory = () => setShowCategoryModal(false)
+
+  useEffect(() => {
+    getCategoriasHook().then(result => setCategorias(result));
+  }, [])
+
+  const formik = useFormik({
+    initialValues: {
+      query: ""
+    },
+    onSubmit: values => {
+      getCategoriasHook(values.query).then(result => setCategorias(result))
+    }
+  })
 
   return (
     <MainContainer>
       <h5>Categorías</h5>
       <Card >
         <Card.Body>
-          <Form className="row row-cols-auto g-2">
+          <Form onSubmit={formik.handleSubmit} className="row row-cols-auto g-2">
             <div className="col-auto col-md-4">
               <InputGroup className="mb-3">
                 <InputGroup.Text>
                   <FontAwesomeIcon icon={faMagnifyingGlass} className="mx-2" />
                 </InputGroup.Text>
                 <Form.Control
+                  name="query"
+                  value={formik.values.query}
+                  onChange={formik.handleChange}
                   placeholder="Nombre"
                 />
               </InputGroup>
             </div>
             <div className="col-auto">
-              <Button variant="success" onClick={handleShowCategory}>
+              <Button type="button" variant="success" onClick={handleShowCategory}>
                 <FontAwesomeIcon icon={faPlus} className="me-1" />
                 <span className="text-uppercase">Agregar Categoría</span>
               </Button>
             </div>
           </Form>
           <div className="table-responsive">
-            <table className="table mx-auto">
+            <table className="table mb-0">
               <thead>
                 <tr className="text-uppercase">
                   <th>N°</th>
@@ -42,7 +61,14 @@ export const ListaCategorias = () => {
                 </tr>
               </thead>
               <tbody>
-
+                {
+                  categorias && categorias.results?.map((item, index) => (
+                    <tr key={item.id} className="text-uppercase">
+                      <td>{index + 1}</td>
+                      <td>{item.nombre}</td>
+                    </tr>
+                  ))
+                }
               </tbody>
             </table>
           </div>
--- src/components/inventarios/productos/Listasubcategorias.jsx
+++ src/components/inventarios/productos/Listasubcategorias.jsx
@@ -2,38 +2,58 @@ import { Card, Form, InputGroup, Button } from "react-bootstrap"
 import { MainContainer } from "../../common/MainContainer"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { SubCategoriaModal } from "./SubCategoriaModal"
+import { getSubCategoriasHook } from "../../../hooks/subcategorias"
+import { useFormik } from "formik"
 
 export const ListaSubCategorias = () => {
-  const [showSubcategoryModal, setShowSubcategoryModal] = useState(false);
-  const handleShowSubcategory = () => setShowSubcategoryModal(true);
-  const handleCloseSubcategory = () => setShowSubcategoryModal(false);
+  const [subCategorias, setSubCategorias] = useState({})
+  const [showSubcategoryModal, setShowSubcategoryModal] = useState(false)
+  const handleShowSubcategory = () => setShowSubcategoryModal(true)
+  const handleCloseSubcategory = () => setShowSubcategoryModal(false)
+
+  useEffect(() => {
+    getSubCategoriasHook().then(result => setSubCategorias(result))
+  }, [])
+
+  const formik = useFormik({
+    initialValues: {
+      query: ""
+    },
+    onSubmit: values => {
+      getSubCategoriasHook(values.query).then(result => setSubCategorias(result))
+    }
+  })
+
   return (
     <MainContainer>
       <h5>SubCategorias</h5>
       <Card >
         <Card.Body>
-          <Form className="row row-cols-auto g-2">
+          <Form onSubmit={formik.handleSubmit} className="row row-cols-auto g-2">
             <div className="col-auto col-md-4">
               <InputGroup className="mb-3">
                 <InputGroup.Text>
                   <FontAwesomeIcon icon={faMagnifyingGlass} className="mx-2" />
                 </InputGroup.Text>
                 <Form.Control
+                  name="query"
+                  value={formik.values.query}
+                  onChange={formik.handleChange}
                   placeholder="Nombre"
                 />
               </InputGroup>
             </div>
             <div className="col-auto">
-              <Button variant="success" onClick={handleShowSubcategory} className="ms-2">
+              <Button type="button" variant="success" onClick={handleShowSubcategory} className="ms-2">
                 <FontAwesomeIcon icon={faPlus} className="me-1" />
                 <span className="text-uppercase">Agregar Subcategoría</span>
               </Button>
             </div>
           </Form>
           <div className="table-responsive">
-            <table className="table">
+            <table className="table mb-0">
               <thead>
                 <tr className="text-uppercase">
                   <th>N°</th>
@@ -41,7 +61,14 @@ export const ListaSubCategorias = () => {
                 </tr>
               </thead>
               <tbody>
-
+                {
+                  subCategorias && subCategorias.results?.map((item, index) => (
+                    <tr key={item.id} className="text-uppercase">
+                      <td>{index + 1}</td>
+                      <td>{item.nombre}</td>
+                    </tr>
+                  ))
+                }
               </tbody>
             </table>
           </div>
