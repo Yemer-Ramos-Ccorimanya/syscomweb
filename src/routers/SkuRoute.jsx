5249953cd41c2e0920323f3acabe4a252d7cb8ff
Yemer-Ramos-Ccorimanya <1005920191@unajma.edu.pe>
2024-01-27 18:23:39 -0500

    se agrego buscar SKU

--- src/routers/SkuRoute.jsx
+++ src/routers/SkuRoute.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import { ListadoSkus } from "../components/ListadoSku/ListadoSku"
-
+import { BuscarSku } from "../components/inventarios/sku/BuscarSku"
 export const SkuRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<ListadoSkus />} />
+      <Route path="/sku/BuscarSkau" element={<BuscarSku />} />
     </Routes>
   )
 }
